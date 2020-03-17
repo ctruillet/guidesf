71f950bc630940fe57832648ba15516ec11e0da9
Pierre Weyl <pie.weyl@gmail.com>
2020-03-17 17:20:42 +0100

    Update link message to TauxSF

--- template/base.php
+++ template/base.php
@@ -57,7 +57,7 @@
 
         <p class="text-muted text-center mt-4 mb-0">Développé avec &hearts; par <a href="http://pwbzh.fr">Pierre Weyl</a>, bénévole chez les <a href="https://www.sgdf.fr/">Scouts et Guides de France</a>.</p>
         <p class="text-muted text-center mb-2">Ce site web n'est pas officiel. Il n'est pas édité par le Scoutisme Français.</p>
-        <p class="text-muted text-center">Connaissez-vous le site web <a href="https://tauxsf.pwbzh.fr">TauxSF</a> ?</p>
+        <p class="text-muted text-center">Tu aimes ce site ? Tu aimeras sûrement <a href="https://tauxsf.pwbzh.fr">TauxSF</a> !</p>
     </div>
 </body>
 
