1a7744edc8db998c62bf8e9e56e68eef06c829b5
Pierre Weyl <pweyl@thalos.fr>
2021-10-04 16:56:12 +0200

    Add analytics and RGPD form

--- template/base.php
+++ template/base.php
@@ -27,6 +27,30 @@
 </head>
 
 <body>
+    <div class="modal fade" id="analyticsModal" tabindex="-1" aria-labelledby="analyticsModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="analyticsModalLabel">Consentement </h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <p>Bonjour,</p>
+                    <p>
+                        J'utilise un cookie pour connaitre le nombre de visiteurs sur le site web GuideSF.<br>
+                        Conformément au RGPD, je vous laisse le choix d'accepter ou non ce suivi. Votre choix est conservé 3 mois.<br>
+                        Vous pouvez modifier votre choix à tout moment en cliquant sur le bouton en bas de la page.
+                    </p>
+                    <p>Merci,<br>Pierre</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-success text-white" id="btn-analytics-ok" data-bs-dismiss="modal">Accepter</button>
+                    <button type="button" class="btn btn-outline-danger" id="btn-analytics-nok" data-bs-dismiss="modal">Refuser</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <div id="website-title" class="bg-dark py-1 px-3 border-bottom"><a href="/" class="text-decoration-none" title="Accueil"><span class="text-white"><strong>GuideSF</strong></span> <span class="text-light"><small>Guide Réglementaire du Scoutisme Français</small></span></a></div>
 
     <?php include_once 'nav.php'; ?>
@@ -57,12 +81,75 @@
 
     <script src="js/bootstrap.bundle.min.js"></script>
     <script>
+        // Tooltip
         document.addEventListener("DOMContentLoaded", function() {
             var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
             var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
                 return new bootstrap.Tooltip(tooltipTriggerEl)
             })
         });
+
+        // Analytics
+        function setCookie(cname, cvalue, exdays) {
+            const d = new Date();
+            d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+            let expires = "expires="+d.toUTCString();
+            document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+        }
+
+        function getCookie(cname) {
+            let name = cname + "=";
+            let decodedCookie = decodeURIComponent(document.cookie);
+            let ca = decodedCookie.split(';');
+            for(let i = 0; i <ca.length; i++) {
+                let c = ca[i];
+                while (c.charAt(0) == ' ') {
+                    c = c.substring(1);
+                }
+                if (c.indexOf(name) == 0) {
+                    return c.substring(name.length, c.length);
+                }
+            }
+            return "";
+        }
+
+        var analyticsModal = new bootstrap.Modal(document.getElementById('analyticsModal'));
+        var allow_analytics = getCookie("allow_analytics");
+
+        if (allow_analytics == "no") {
+            // Do nothing
+        } else if (allow_analytics == "yes") {
+            // Enable analytics
+            var analyticsID = "<?php echo getenv('ANALYTICS_ID'); ?>";
+
+            let myScript = document.createElement("script");
+            myScript.setAttribute("src", "https://www.googletagmanager.com/gtag/js?id="+analyticsID);
+            document.body.appendChild(myScript);
+
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', analyticsID);
+        } else {
+            // Open modal
+            analyticsModal.show();
+        }
+
+        document.getElementById('btn-analytics-ok').onclick = function() {
+            // Allow analytics
+            setCookie("allow_analytics", "yes", 92);
+        };
+
+        document.getElementById('btn-analytics-nok').onclick = function() {
+            // Disallow analytics
+            setCookie("allow_analytics", "no", 92);
+        };
+
+        document.getElementById('btn-analytics-open-modal').onclick = function() {
+            // Open modal
+            analyticsModal.show();
+        };
     </script>
 </body>
 
--- template/footer.php
+++ template/footer.php
@@ -1,9 +1,11 @@
 <p id="source">Source : <a href="http://www.scoutisme-francais.fr/formation" target="_blank">Guide Réglementaire du Scoutisme Français - Édition du 5 février 2020</a></p>
 
 <div class="text-center bg-white p-3 border border-secondary">
-    <p class=" mb-0">Ce site web n'est pas officiel. Il n'est pas édité par le Scoutisme Français.</p>
-    <p class="">Développé avec &hearts; par <a href="mailto:pie.weyl@gmail.com" data-bs-toggle="tooltip" title="pie.weyl@gmail.com">Pierre Weyl</a>, bénévole chez les <a href="https://www.sgdf.fr/" target="_blank">Scouts et Guides de France</a>.</p>
+    <p class="mb-0">Ce site web n'est pas officiel. Il n'est pas édité par le Scoutisme Français.</p>
+    <p>Développé avec &hearts; par <a href="mailto:pie.weyl@gmail.com" data-bs-toggle="tooltip" title="pie.weyl@gmail.com">Pierre Weyl</a>, bénévole chez les <a href="https://www.sgdf.fr/" target="_blank">Scouts et Guides de France</a>.</p>
 
-    <p class=" mb-0">Tu souhaites contribuer ? Cette application est un logiciel libre. Retrouvons-nous sur <a href="https://github.com/pwbzh/guidesf" target="_blank">GitHub</a> :-)</p>
-    <p class=" mb-0">Contributeur : <a href="https://github.com/antoinevth" target="_blank">antoinevth</a></p>
+    <p class="mb-0">Tu souhaites contribuer ? Cette application est un logiciel libre. Retrouvons-nous sur <a href="https://github.com/pwbzh/guidesf" target="_blank">GitHub</a> :-)</p>
+    <p>Contributeur : <a href="https://github.com/antoinevth" target="_blank">antoinevth</a></p>
+
+    <p class="mb-0"><button type="button" class="btn btn-outline-secondary btn-sm" id="btn-analytics-open-modal">Formulaire de consentement sur le suivi des visites</button></p>
 </div>
